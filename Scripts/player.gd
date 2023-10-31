d58d41aecbd1609c716b2f7e2cc3786a33437770
Francis Gauthier <fgauthier@crc-lennox.qc.ca>
2023-10-31 10:07:35 -0400

    Added camera and fixed the player rotation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
+class_name Player
 
+@export var speed: float = 15;
+@export var jump_impulse: float = 7;# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
